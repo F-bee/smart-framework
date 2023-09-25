008a12c38a25ffced530b630ec362423a182b8e3
fkxia <615929180@qq.com>
2023-09-25 22:51:35 +0800

    feat(util、helper): 实现依赖注入功能
    IoC依赖注入助手类，数组工具类

---
+++ src/main/java/com/kk/framework/helper/ArrayUtil.java
@@ -0,0 +1,25 @@
+package com.kk.framework.helper;
+
+import org.apache.commons.lang3.ArrayUtils;
+
+/**
+ * @author: fkxia
+ * @description 数组工具类
+ * @date: 2023/9/25 22:35
+ */
+public final class ArrayUtil {
+
+    /**
+     * 判断数组是否为空
+     */
+    public static boolean isEmpty(Object[] array) {
+        return ArrayUtils.isEmpty(array);
+    }
+
+    /**
+     * 判断数组是否非空
+     */
+    public static boolean isNotEmpty(Object[] array) {
+        return !ArrayUtils.isEmpty(array);
+    }
+}
